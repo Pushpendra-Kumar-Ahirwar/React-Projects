7ef219b02fde8e5e703fbd459eba9aa8ee65db14
Pushpendra Kumar Ahirwar <rajpushpendra19@gmail.com>
2024-07-02 23:00:54 +0530

    Modify TodoSlice.js file

--- TodoUsingRedux-Toolkit/src/features/todo/TodoSlice.js
+++ TodoUsingRedux-Toolkit/src/features/todo/TodoSlice.js
@@ -19,6 +19,7 @@ export const TodoSlice = createSlice({
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
+
     }
 })
 
